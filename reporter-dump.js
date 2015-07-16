dcfece58dcfb2ff9b111426467791d7fc3127a26
Em <Rumanti@users.noreply.github.com>
2015-07-16 19:59:20 +0700

    update

--- reporter-dump.js
+++ reporter-dump.js
@@ -10,16 +10,34 @@
     
     // Slice 
     ext.slice = function(input, first, second) {
-        return input.slice(first,second); 
+        return input.slice(first, second); 
     }; 
+    
+    // Reverse
+    ext.reverse = function(input) {
+        return input.reverse();
+    };
+    
+    // Upper
+    ext.upper = function(input) {
+        return input.toUpperCase();
+    };
+    
+    // Lower
+    ext.lower = function(input) {
+        return input.toLowerCase();
+    }
     
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-          ['r', 'slice of %s from %n to %n', 'slice', 'hello', 1, 5] 
+          ['r', 'slice of %s from %n to %n', 'slice', 'hello', 1, 5],
+          ['r', 'reverse %s' 'reverse' 'rotator']
+          ['r' 'upper %s' 'upper' 'sCrAtCh'],
+          ['r' 'lower %s' 'lower' 'ScRaTcH']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Reporter Dump', descriptor, ext);
 })({});

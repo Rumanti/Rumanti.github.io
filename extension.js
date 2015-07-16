f52bd5e2ad1ae1ca7791f3c2aaa659298322af54
Em <Rumanti@users.noreply.github.com>
2015-07-16 16:07:41 +0700

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,25 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    
+    // Slice block
+    ext.slice = function(input, first, second) {
+        return input[first:second];
+    }
+
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+          ['r', 'slice of %s from %n to %n', slice, 'hello', 1, 5]
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
